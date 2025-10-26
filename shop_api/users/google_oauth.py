34f7897ba9768484429c7fbf5abe75033fd221d1
jjeetaev <kat91971kara@gmail.com>
2025-10-26 16:18:28 +0600

    hw4

--- shop_api/users/google_oauth.py
+++ shop_api/users/google_oauth.py
@@ -21,7 +21,7 @@ def get_google_user_info(auth_code, redirect_uri):
     headers = {"Authorization": f"Bearer {access_token}"}
     r = requests.get(user_info_url, headers=headers)
     r.raise_for_status()
-    return r.json()  # вернёт dict с email, given_name, family_name
+    return r.json()  
 
 def get_or_create_user(google_user):
     email = google_user["email"]
@@ -31,13 +31,25 @@ def get_or_create_user(google_user):
             "first_name": google_user.get("given_name", ""),
             "last_name": google_user.get("family_name", ""),
             "created_at": timezone.now(),
+            "is_active": True,
+            "registration_source": "google",
         }
     )
+
+    user.first_name = google_user.get("given_name", user.first_name)
+    user.last_name = google_user.get("family_name", user.last_name)
+    user.last_login = timezone.now()
+    user.is_active = True
+    user.registration_source = "google"
+    user.save()
     return user
 
 def generate_jwt_for_user(user):
     refresh = RefreshToken.for_user(user)
     refresh['email'] = user.email
+    refresh['registration_source'] = user.registration_source
+    if user.birthdate:
+        refresh['birthdate'] = str(user.birthdate)
     return {
         "refresh": str(refresh),
         "access": str(refresh.access_token)
